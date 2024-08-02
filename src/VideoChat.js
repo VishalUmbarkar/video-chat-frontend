9e87a560d8bf8d54a505470a79a5aac394df15d7
phoenixisdead <vishalumbarkar11@gmail.com>
2024-08-02 14:50:25 +0530

    stable chanes

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -1,203 +1,151 @@
 import React, { useEffect, useRef } from 'react';
 
 const WebRTCComponent = () => {
-  // const localVideoRef = useRef(null);
-  // const remoteVideoRef = useRef(null);
-  // const [localStream, setLocalStream] = useState(null);
   const ws = useRef(null);
+  const peerConnection = useRef(null);
 
-  //websocket creation
-  ws.current = new WebSocket('ws://video-chat-app-production-24b6.up.railway.app/video-chat');
+  useEffect(() => {
+    // WebSocket creation
+    ws.current = new WebSocket('ws://video-chat-app-production-24b6.up.railway.app/video-chat');
 
-  //PeerConnection Creation
-  const peerConnection = new RTCPeerConnection({
-    iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-  });
+    ws.current.onopen = () => {
+      console.log("WebSocket online");
+    };
 
+    ws.current.onclose = () => {
+      console.log("WebSocket offline");
+    };
 
-//-------------------Utility Functions-----------------------------------
+    ws.current.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      console.log("Received message:", message);
 
-const sendToSocket =(message)=>{
-  console.log("Sending msg to socket:", message);
-  ws.current.send(JSON.stringify({message}));
-}
+      if (message.type === 'offer') {
+        handleOffer(message);
+      } else if (message.type === 'answer') {
+        handleAnswer(message);
+      } else if (message.type === 'new-ice-candidate') {
+        handleNewICECandidateMsg(message);
+      }
+    };
 
-
- async function handleOffer(message) {
-  console.log(message);
-  const obj = message.message;
-  console.log("obj: ",obj)
-  console.log("message type: ", obj.type);
-  if(obj.type==="offer"){
-  const rtcSessionDescription = new RTCSessionDescription({
-    type: obj.sdp.type,
-    sdp: obj.sdp.sdp
-  });
-  await peerConnection
-    .setRemoteDescription(rtcSessionDescription)
-    .then(() => navigator.mediaDevices.getUserMedia({audio:"false", video:"true"}))
-    // .then((stream) => {
-    //   document.getElementById("local_video").srcObject = stream;
-    //   return peerConnection.addStream(stream);
-    // })
-    .then(() => peerConnection.createAnswer())
-    .then((answer)=>console.log("created answer is : ", answer))
-    .then((answer) => 
-      peerConnection.setLocalDescription(answer))
-    .then(() => {
-      // Send the answer to the remote peer using the signaling server
-      sendToSocket({type: "anwer", sdp: peerConnection.localDescription});
-      console.log(peerConnection.localDescription)
-    })
-    .catch("handleGetUserMediaError");
-  }
-  else if(obj.type==="anwer"){
-    const rtcSessionDescription = new RTCSessionDescription({
-      type: obj.sdp.type,
-      sdp: obj.sdp.sdp
+    // PeerConnection Creation
+    peerConnection.current = new RTCPeerConnection({
+      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
     });
-  
-    await peerConnection
-      .setRemoteDescription(rtcSessionDescription)
-      .then(() => {
-        console.log("Remote description set successfully with answer: ", rtcSessionDescription);
-      })
-      .catch((error) => {
-        console.error('Error setting remote description with answer:', error);
+
+    peerConnection.current.onicecandidate = (ev) => {
+      if (ev.candidate) {
+        console.log("Sending new ice candidate to socket server: ", ev.candidate);
+        sendToSocket({
+          type: "new-ice-candidate",
+          candidate: ev.candidate,
+        });
+      }
+    };
+
+    peerConnection.current.addEventListener("icegatheringstatechange", (ev) => {
+      switch (peerConnection.current.iceGatheringState) {
+        case "new":
+          console.log("new");
+          break;
+        case "gathering":
+          console.log("gathering");
+          break;
+        case "complete":
+          console.log("complete");
+          break;
+        default:
+          console.log("nothing");
+      }
+    });
+
+    peerConnection.current.onnegotiationneeded = (ev) => {
+      peerConnection.current.createOffer()
+        .then((offer) => peerConnection.current.setLocalDescription(offer))
+        .then(() =>
+          sendToSocket({
+            type: "video-offer",
+            sdp: peerConnection.current.localDescription,
+          }),
+        )
+        .catch((err) => {
+          // handle error
+          console.log("error while handling negotiation : ", err)
+        });
+    };
+
+    return () => {
+      ws.current.close();
+    };
+  }, []);
+
+
+  //----------------------------functions---------------------------------------------------
+
+  const sendToSocket = (message) => {
+    console.log("Sending msg to socket:", message);
+    ws.current.send(JSON.stringify(message));
+  };
+
+//----------handleOffer starts
+
+  const handleOffer = async (message) => {
+    console.log("Handling offer:", message);
+
+    const rtcSessionDescription = new RTCSessionDescription(message.sdp);
+    await peerConnection.current.setRemoteDescription(rtcSessionDescription);
+
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+    stream.getTracks().forEach(track => peerConnection.current.addTrack(track, stream));
+
+    const answer = await peerConnection.current.createAnswer();
+    await peerConnection.current.setLocalDescription(answer);
+
+    sendToSocket({
+      type: "answer",
+      sdp: peerConnection.current.localDescription
+    });
+  };
+
+ //-----------handleAnswer starts
+
+  const handleAnswer = async (message) => {
+    console.log("Handling answer:", message);
+
+    const rtcSessionDescription = new RTCSessionDescription(message.sdp);
+    await peerConnection.current.setRemoteDescription(rtcSessionDescription);
+  };
+
+  const handleNewICECandidateMsg = async (message) => {
+    console.log("iceCandidate : ", message.candidate);
+    const candidate = new RTCIceCandidate(message.candidate);
+    await peerConnection.current.addIceCandidate(candidate);
+  };
+
+  //-----------makeCall starts
+
+  const makeCall = async () => {
+    console.log("Call Initiated");
+
+    try {
+      const offer = await peerConnection.current.createOffer();
+      await peerConnection.current.setLocalDescription(offer);
+
+      sendToSocket({
+        type: "offer",
+        sdp: peerConnection.current.localDescription
       });
-  }
-}
-
-async function handleAnswer(message) {
-  console.log(message);
-  const obj = message.message;
-  console.log("obj: ", obj);
-  const rtcSessionDescription = new RTCSessionDescription({
-    type: obj.sdp.type,
-    sdp: obj.sdp.sdp
-  });
-
-  await peerConnection
-    .setRemoteDescription(rtcSessionDescription)
-    .then(() => {
-      console.log("Remote description set successfully with answer: ", rtcSessionDescription);
-    })
-    .catch((error) => {
-      console.error('Error setting remote description with answer:', error);
-    });
-} 
-
-//-------------------Web Socket Events---------------------------------------
-
-  ws.current.onopen = ()=>{
-    console.log("Websocket online");
-  }
-
-  ws.current.onclose = ()=>{
-    console.log("Websocket offline");
-  }
-
-  useEffect(()=>{
-  ws.current.onmessage = (event) => {
-    const receivedString = event.data;
-    const type = JSON.stringify(receivedString);
-    console.log(type);
-    console.log("received string: ",receivedString);
-    const message = JSON.parse(receivedString);
-    console.log(message.type);
-    handleOffer(message);
-    
-    
-    
-  }
-},[ws.current])
-
-  // ws.current.onmessage = (event) => {
-  //   const receivedString = event.data;
-  //   console.log("received string: ",receivedString);
-  //   const message = JSON.parse(receivedString);
-  //   if (message.type === "answer") {
-  //     handleAnswer(message);
-  //   }
-    
-  // }
-
-//----------------------------------------------------------------------------
-
-//---------------------Peer Connection Events---------------------------------
-
-peerConnection.onicecandidate = (ev) => {
-  if (ev.candidate !== null) {
-    sendToSocket({
-      type: "new-ice-candidate",
-      candidate: ev.candidate,
-    });
-  }
-};
-
-
-peerConnection.addEventListener("icegatheringstatechange", (ev) => {
-  switch (peerConnection.iceGatheringState) {
-    case "new":
-      /* gathering is either just starting or has been reset */
-      console.log("new")
-      break;
-    case "gathering":
-      /* gathering has begun or is ongoing */
-      console.log("gathering")
-      break;
-    case "complete":
-      /* gathering has ended */
-      console.log("completed")
-      break;
-     default:
-      console.log("nothing")
-  }
-});
-
-
-peerConnection.onicecandidate = (ev) => {
-  if(ev.candidate){
-    console.log("candidate is null");
-  }
-  if (ev.candidate !== null) {
-    console.log("Sending new ice candidate to socket server: ", ev.candidate);
-    sendToSocket({
-      type: "new-ice-candidate",
-      candidate: ev.candidate,
-    });
-  }
-};
-
-
-
-const makeCall = ()=>{
-  console.log("Call Initiated");
-  try {
-    peerConnection.createOffer()
-    .then((offer)=> peerConnection.setLocalDescription(offer))
-    .then(()=> {
-      sendToSocket({type: "offer", sdp: peerConnection.localDescription,});
-      // console.log("localDescription: ", localDescription);
-    })
-    // console.log("Offer created: ", offer);
-  } catch (error) {
-    console.warn("Error while creating local offer : ", error);
-  }
-  
-}
-
+    } catch (error) {
+      console.warn("Error while creating local offer: ", error);
+    }
+  };
 
   return (
     <div>
-      {/* <video ref={localVideoRef} autoPlay muted></video> */}
-      {/* <video ref={remoteVideoRef} autoPlay></video> */}
       <button onClick={makeCall}>Start Call</button>
     </div>
   );
 };
 
 export default WebRTCComponent;
-
-
-
